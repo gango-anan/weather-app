37b099cb11af49b4ba9d272566e33117eadc82f2
Galiwango Ananiya <galiwango101@gmail.com>
2021-05-21 00:14:44 +0300

    Add functionality to toggle the temperature display.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import apiDataModule from './fetch_weather';
 import utilitiesModule from './utilities';
 import renderWeatherData from './render';
 
-
+const tempToggler = document.getElementById('temp');
+const tempTogglerLabel = document.querySelector('.temp-label');
 const utilityKey = utilitiesModule.utilityKey().apiKey;
 const weatherData = apiDataModule.getData('kampala', utilityKey);
+
+const toggleTemperature = () => {
+  if(tempToggler.checked) {
+    renderWeatherData.displayDetails(weatherData);
+  }else{
+    renderWeatherData.displayDetails(weatherData);
+  }
+}
+
+// Event Listeners
+tempTogglerLabel.addEventListener('click', (event) => {
+  setTimeout(toggleTemperature, 10);
+})
+
+
 
 renderWeatherData.displayDetails(weatherData);
