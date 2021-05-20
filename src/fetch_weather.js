25166d4deed710ee0e8ba9a7555fdf1b15b3898c
Galiwango Ananiya <galiwango101@gmail.com>
2021-05-20 20:40:21 +0300

    Refactor fetch_weather into a module to return an object with the required weather details.

--- src/fetch_weather.js
+++ src/fetch_weather.js
@@ -1,1 +1,30 @@
+const apiDataModule = (() => {
+  const generateObject = (data) => {
+    return {
+      temperature: data.main.temp,
+      visibility: data.visibility,
+       humidity: data.main.humidity,
+      windSpeed: data.wind.speed,
+       city: data.name,
+       country: data.sys.country,
+       icon: data.weather[0].icon
+    }
+  }
 
+   const getData = async (city, utilityKey) => {
+    try {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${utilityKey}`, {
+        mode: 'cors',
+      });
+      const data = await response.json();
+      return generateObject(data);
+    } catch (e) {
+      return setTimeout(console.log('invalid city'), 200);
+    }
+  }
+
+  return { getData }
+
+})();
+
+export { apiDataModule as default };
