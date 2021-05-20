ce665a9fbca77aeb1da49f7ce7cce3a18d9a5f81
Galiwango Ananiya <galiwango101@gmail.com>
2021-05-21 00:58:26 +0300

    Add city search functionality.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import apiDataModule from './fetch_weather';
 import utilitiesModule from './utilities';
 import renderWeatherData from './render';
 
+const cityForm = document.querySelector('form');
+const cityFormInput = document.getElementById('city-search');
 const tempToggler = document.getElementById('temp');
 const tempTogglerLabel = document.querySelector('.temp-label');
 const utilityKey = utilitiesModule.utilityKey().apiKey;
-const weatherData = apiDataModule.getData('kampala', utilityKey);
+let city = 'kampala'
+let weatherData = apiDataModule.getData(city, utilityKey);
 
 const toggleTemperature = () => {
   if(tempToggler.checked) {
@@ -16,5 +19,11 @@ const toggleTemperature = () => {
 };
 
 tempTogglerLabel.addEventListener('click', () => setTimeout(toggleTemperature, 10))
+cityForm.addEventListener('submit', event => {
+  event.preventDefault();
+  city = cityFormInput.value
+  weatherData = apiDataModule.getData(city, utilityKey);
+  renderWeatherData.displayDetails(weatherData);
+})
 
 renderWeatherData.displayDetails(weatherData);
