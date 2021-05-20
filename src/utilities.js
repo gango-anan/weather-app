da6b655070c472c84ce16d33c0d6aa8fbe2f1b7b
Galiwango Ananiya <galiwango101@gmail.com>
2021-05-20 20:36:34 +0300

    Add utility function to store and return apiKey whenever its needed by the application.

--- src/utilities.js
+++ src/utilities.js
@@ -1,10 +1,16 @@
 const utilitiesModule = (() => {
-   const getAndRenderCurrentDate = (dateContainer) => {
+  const utilityKey = () => {
+    const apiKeyValue = 'af42f1d1d1cde88bd430cbdaf2d5e6cd';
+  
+    return { apiKey: apiKeyValue };
+  };
+  
+  const getAndRenderCurrentDate = (dateContainer) => {
     const now_date = new Date();
     dateContainer.innerText = `${now_date.toLocaleTimeString()} , ${now_date.toDateString()}`
   };
 
-  return { getAndRenderCurrentDate }
+  return { utilityKey, getAndRenderCurrentDate }
 })();
 
 
